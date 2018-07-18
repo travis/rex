e4d7eb7fa06ab339b13a85c4825382d0fd822571
Travis Vachon <travis.vachon@gmail.com>
2018-07-17 20:58:41 -0700

    Add Claim type to graphql schema

--- api/src/server.js
+++ api/src/server.js
@@ -3,15 +3,37 @@ import { ApolloServer, gql } from 'apollo-server-express'
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Claim {
+    id: ID
+    url: String
+    title: String
+    authorName: String
+    authorURL: String
+    dateEdited: String
+  }
+
   type Query {
-    hello: String
+    claims: [Claim]
   }
 `;
+
+const claims = [{id: 1,
+                 url: "bacon_is_best",
+                 title: "Bacon is best",
+                 authorName: "Ian",
+                 authorURL: "ian",
+                 dateEdited: "2018-07-18T03:43:53+00:00"},
+                {id: 2,
+                 url: "ham_is_best",
+                 title: "Ham is best",
+                 authorName: "Tani",
+                 authorURL: "tani",
+                 dateEdited: "2018-08-12T06:03:12+00:00"}]
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    claims: () => claims,
   },
 };
 
