51e9d01a6544384b8b9ac82a94de9a0297624aff
Travis Vachon <travis.vachon@gmail.com>
2018-07-17 21:22:07 -0700

    Add sequelize, run init
    npx sequelize init

---
+++ api/src/db/config.js
@@ -0,0 +1,31 @@
+const {
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DEV_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_PROD_DB
+} = process.env;
+
+module.exports = {
+  development: {
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DEV_DB,
+    host: 'db',
+    dialect: 'postgres',
+  },
+  test: {
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_TEST_DB,
+    host: 'db',
+    dialect: 'postgres',
+  },
+  production: {
+    username: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_PROD_DB,
+    host: 'db',
+    dialect: 'postgres',
+  }
+}
