1d42bcf18eb4739767b9e692d6681d0c7cc9b39f
Travis Vachon <travis.vachon@gmail.com>
2018-07-20 10:53:11 -0700

    Add pages to demonstrate user/claim association

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Claims from './Claims'
+import Users from './Users'
+import User from './User'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">welcome to rex</h1>
-        </header>
-        <Claims/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">welcome to rex</h1>
+            <ul>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/claims">Claims</Link>
+              </li>
+            </ul>
+          </header>
+          <Route path="/users" component={Users} />
+          <Route path="/user/:id" component={User} />
+          <Route path="/claims" component={Claims} />
+        </div>
+      </Router>
     );
   }
 }
