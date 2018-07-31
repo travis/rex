b6c1027a89803681853a3abd31d8397bca6f2954
Travis Vachon <travis.vachon@gmail.com>
2018-07-31 15:37:13 -0700

    Add example stories for components

--- ui/src/stories/index.js
+++ ui/src/stories/index.js
@@ -6,6 +6,10 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+import Claims from '../components/Claims'
+import User from '../components/User'
+import Users from '../components/Users'
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
@@ -17,3 +21,15 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('Claims', module)
+  .add('with claims', () => <Claims claims={[{id: 1, title: "Ducks are cool", url: "/ducks_are_cool"}, {id: 2, title: "Geese are neat", url: "/geese_are_neat"}]}>
+  </Claims>)
+
+storiesOf('User', module)
+  .add('with claims', () => <User claims={[{id: 1, title: "Ducks are cool", url: "/ducks_are_cool"}, {id: 2, title: "Geese are neat", url: "/geese_are_neat"}]}>
+       </User>)
+
+storiesOf('Users', module)
+  .add('with users', () => <Users users={[{id: 1, firstName: "Travis", lastName: "Vachon", email: "dontatme@gmail.com"}]} >
+       </Users>)
