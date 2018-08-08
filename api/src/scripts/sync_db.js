036f8bc1346c5a357340a6ac9fb1a0d6e8cbdd97
Travis Vachon <travis.vachon@gmail.com>
2018-08-08 09:54:41 -0700

    Add utils for syncing the DB
    Also add top-level project package.json to start accumulating
    top-level scripts. The DB sync task is a good one, since that should
    naturally live alongside the top level docker-compose infrastructure.

---
+++ api/src/scripts/sync_db.js
@@ -0,0 +1,9 @@
+import commandLineArgs from 'command-line-args'
+import db from '../models'
+
+const { force } = commandLineArgs([
+  { name: 'force', alias: 'f', type: Boolean }
+])
+
+db.sequelize.sync({force: force}).
+  then(() => db.sequelize.close())
