ffea99b1906de439701085376467ef01f6be3803
Travis Vachon <travis.vachon@gmail.com>
2018-08-09 00:52:08 -0700

    Add StoryBook Apollo integration, use it to test ClaimsPage
    Pull in the now-shared schema and use it to mock data at the level of
    the GraphQL APi

--- ui/src/stories/index.js
+++ ui/src/stories/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import StoryRouter from 'storybook-react-router';
+import apolloStorybookDecorator from 'apollo-storybook-react';
 
 import { Button, Welcome } from '@storybook/react/demo';
+
+import typeDefs from 'rex-schema'
 
 import Claims from '../components/Claims'
 import User from '../components/User'
 import Users from '../components/Users'
+import ClaimsPage from '../bindings/ClaimsPage'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -22,14 +27,30 @@ storiesOf('Button', module)
     </Button>
   ));
 
+const claim1 = {id: 1, title: "Ducks are cool", slug: "ducks_are_cool"}
+const claim2 = {id: 2, title: "Geese are neat", slug: "geese_are_neat"}
+const user1 = {id: 1, firstName: "Travis", lastName: "Vachon", email: "dontatme@gmail.com"}
+
+
 storiesOf('Claims', module)
-  .add('with claims', () => <Claims claims={[{id: 1, title: "Ducks are cool", slug: "ducks_are_cool"}, {id: 2, title: "Geese are neat", slug: "geese_are_neat"}]}>
+  .add('with claims', () => <Claims claims={[claim1, claim2]}>
   </Claims>)
 
 storiesOf('User', module)
-  .add('with claims', () => <User claims={[{id: 1, title: "Ducks are cool", slug: "/ducks_are_cool"}, {id: 2, title: "Geese are neat", slug: "/geese_are_neat"}]}>
+  .add('with claims', () => <User claims={[claim1, claim2]}>
        </User>)
 
 storiesOf('Users', module)
-  .add('with users', () => <Users users={[{id: 1, firstName: "Travis", lastName: "Vachon", email: "dontatme@gmail.com"}]} >
+  .add('with users', () => <Users users={[user1]} >
        </Users>)
+
+const withMocks = (mocks) => apolloStorybookDecorator({ typeDefs, mocks})
+
+storiesOf('ClaimsPage', module)
+  .addDecorator(StoryRouter())
+  .addDecorator(withMocks({
+    Query: () => ({
+      claims: () => [claim1, claim2]
+    })
+  }))
+  .add('with claims', () => <ClaimsPage />)
