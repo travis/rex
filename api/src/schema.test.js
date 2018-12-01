000ca8e591a84970ae9695844ff63dac9647be3e
Travis Vachon <travis.vachon@gmail.com>
2018-11-30 16:34:52 -0800

    Fix schema tests
    Update to patterns established for the domain library, use describe
    context to clean up tests

--- api/src/schema.test.js
+++ api/src/schema.test.js
@@ -4,30 +4,18 @@ import { runQuery } from 'apollo-server-core'
 import typeDefs from 'rex-schema'
 import resolvers from './schema/resolvers'
 
+import { setupCleanDatabase, clearDatabase, cleanupDatabaseConnection } from './domain'
+
 import { Claim, User, sequelize } from './models'
 import { travis } from './test/data'
 
 const schema = makeExecutableSchema({typeDefs, resolvers})
-const queryResult = (query, vars) =>
-      graphql(schema, query, null, null, vars)
 
-beforeAll(() => {
-  return sequelize.sync({force: true})
-});
+beforeAll(setupCleanDatabase)
 
-beforeEach(() => {
-  return Claim.bulkCreate([
-    { title: 'Bar' },
-  ])
-});
+afterEach(clearDatabase)
 
-afterEach(() => {
-  return Claim.destroy({truncate: true})
-});
-
-afterAll(() => {
-  return sequelize.close()
-})
+afterAll(cleanupDatabaseConnection)
 
 describe("with an existing User", () => {
   let user;
@@ -36,21 +24,29 @@ describe("with an existing User", () => {
     user = savedTravis
   }))
 
-  const queryResult = async (queryString, variables, currentUser) =>
+  const queryResult = async (queryString, variables, currentUser=travis) =>
         runQuery({schema, queryString, variables,
                   debug: true,
                   context: {
                     currentUser
                   }})
-test('addClaim creates a unit', async () => {
-  await expect(queryResult('mutation AddClaim($title: String!, $authorID: ID) {addClaim(title: $title, authorID: $authorID) {slug, title}}', {title: "Foo"})).resolves.toEqual(
-    {data: {addClaim: {title: "Foo", slug: "foo"}}}
-  );
-});
 
-test('claims returns claims', async () => {
-  await expect(queryResult('{claims { title, slug }}')).resolves.toEqual(
-    {data: {claims: [{title: "Bar", slug: "bar"}]}}
-  );
-});
+  test('addClaim creates a claim', async () => {
+    await expect(queryResult('mutation AddClaim($title: String!, $authorID: ID) {addClaim(title: $title, authorID: $authorID) {slug, title}}', {title: "Foo"})).resolves.toEqual(
+      {data: {addClaim: {title: "Foo", slug: "foo"}}}
+    );
+  });
+
+  describe('with a claim', async () => {
+    let claim;
+    beforeEach(() => Claim.create({title: "Bar"}).then((savedClaim) => {
+      claim = savedClaim;
+    }))
+
+    test('claims returns claims', async () => {
+      await expect(queryResult('{claims { title, slug }}')).resolves.toEqual(
+        {data: {claims: [{title: "Bar", slug: "bar"}]}}
+      );
+    });
+  })
 })
